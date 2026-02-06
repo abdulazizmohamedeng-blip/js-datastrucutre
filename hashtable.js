325d896f7a6d2490fa95fe10cf8561c5a5a3fbe1
Abdulaziz Mohamed <abdulazizmohamedeng@gmail.com>
2026-02-07 01:55:11 +0200

    create keys method in hash table to get all keys

--- hashtable.js
+++ hashtable.js
@@ -2,6 +2,7 @@ class hashtable {
   constructor(size) {
     this.data = new Array(size);
   }
+
   _hash(key) {
     let hash = 0;
     for (let i = 0; i < key.length; i++) {
@@ -9,6 +10,7 @@ class hashtable {
     }
     return hash;
   }
+
   set(key, value) {
     let address = this._hash(key);
     if (!this.data[address]) {
@@ -29,6 +31,16 @@ class hashtable {
     }
     return undefined;
   }
+
+  keys() {
+    let keys = [];
+    for (let i = 0; i < this.data.length; i++) {
+      for (let j = 0; j < this.data[i].length; j++) {
+        keys.push(this.data[i][j][0]);
+      }
+    }
+    return keys;
+  }
 }
 
 let tb = new hashtable(2);
@@ -37,5 +49,20 @@ tb.set("banana", 30);
 tb.set("orange", 40);
 tb.set("strawberry", 50);
 tb.set("kuwi", 40);
+tb.set("kuwi", 4500);
 
-console.log(tb);
+console.log(tb.keys());
+
+function firstRecurring(arr) {
+  let hashtable = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (hashtable[arr[i]] == undefined) {
+      hashtable[arr[i]]++;
+    } else {
+      return arr[i];
+    }
+  }
+  return undefined;
+}
+
+console.log(firstRecurring([2, 3, 4, 5]));
